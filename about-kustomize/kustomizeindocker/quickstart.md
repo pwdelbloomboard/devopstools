a6c55c6939f7d03f154f111ad9b507d5f7424c29
Patrick Delaney <patrick.delaney@bloomboard.com>
2022-05-11 13:07:59 -0500

    Update Dockerfile, quickstart, startfiles, commandline examples, etc.

--- about-kustomize/kustomizeindocker/quickstart.md
+++ about-kustomize/kustomizeindocker/quickstart.md
@@ -17,17 +17,19 @@ Then do:
 ./composeexec.sh
 ```
 
-## Applying the Settings
+## Applying the Settings with Kubectl
 
 * Settings get pulled down from a repo within our dockerfile, [k3d-demo](https://github.com/pwdelbloomboard/k3d-demo).
-* Once this is cloned into our cluster via the ./composeexec.sh script, then we can manually apply the settings to launch the app with:
+* Once this is cloned into our cluster via the ./composeexec.sh script, the settings are automatically updated with the docker-entrypoint.sh script.
 
-```
-kubectl apply --filename k8s/
-```
+
+## Applying Kustomization.yaml
+
+
 
 
 ## Visiting the Site
 
+Once the pod has finished with its setup, you can visit the site at:
 
 http://localhost:8081/
--- about-kustomize/kustomizeindocker/startfiles/docker-entrypoint.sh
+++ about-kustomize/kustomizeindocker/startfiles/docker-entrypoint.sh
@@ -2,11 +2,36 @@
 
 # we are starting out in the directory /home/k3d-demo/ as specified in the dockerfile
 
-# create a test cluster
-k3d cluster create testcluster --api-port 6550 -p "8081:80@loadbalancer"
+# create a test cluster, append errors and results to results.txt
+k3d cluster create testcluster --api-port 6550 -p "8081:80@loadbalancer" 2>&1 | tee -a results.txt
 
-# once test cluster is up, apply pre-downloaded settings
-kubectl apply --filename k8s/
+# once test cluster is up, apply pre-downloaded settings, append errors and results to results.txt
+kubectl apply --filename k8s/ 2>&1 | tee -a results.txt
+
+# add the current annotations to the results.txt
+echo -e "\n ------------------------------------------" >> results.txt
+
+echo -e "\n --- Annotations Prior to Kustomization ---" >> results.txt
+
+echo -e "\n deployment \n " >> results.txt
+
+kubectl describe deployment | grep -n Annotations | tee -a results.txt
+
+echo -e "\n ------------------------------------------" >> results.txt
+
+# once pre-downloaded settings are complete and within the cluster, apply kustomization, append errors and results to results.txt
+kubectl apply -k k8s/ 2>&1 | tee -a results.txt
+
+# add the current annotations to the results.txt
+echo -e "\n ------------------------------------------" >> results.txt
+
+echo -e "\n ----- Annotations After Kustomization ----" >> results.txt
+
+echo -e "\n deployment \n " >> results.txt
+
+kubectl describe deployment | grep -n Annotations | tee -a results.txt
+
+echo -e "\n ------------------------------------------" >> results.txt
 
 
 # If a command was passed in, use it. Otherwise use /app/start.
